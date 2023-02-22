96bb45ae17b085d8df085c30568ed88e8c90e889
Samiullah Khan <samiullah.khan.1989@gmail.com>
2023-02-22 11:16:06 +0400

    Implement lazy git (#10)
    * removed the lsp from packer and added lualine via lazygit
    
    * removed telescope and added via lazy.nvim
    
    * added onderk theme 🎭
    
    * comments are back
    
    * basic treesitter 🌳 added
    
    * LSP/nvim-cmp snippets are now with lazy.nvim 💪
    
    * closes #8 vimPlug replaced with Lazy.vim
    
    The very first vim plugin manager I cam acrossed was vundle but it
    was messy.
    
    Luckicly there was vimplug. It served it purpose well but it was
    sometime hard to setup. Becuase in tutorials people have used
    packer.nvim and nicely you can initialize the plugin right there.
    
    That was the big motivation but that I found Lazy.nvim that is picking
    up the pace. It was so easy to move from vimplug to Lazy.nvim. I was
    worried it would take month and mindset to transition. But I did it
    three days. Or I can say 8 hours in total.
    
    With this big change changed I used mason for managing lsp changes. That
    I am also very happy with. I kind of ignore/skipped the UI to manage lsp
    but more of writing configuration.
    
    * added package json

---
+++ lua/lazyplugs.lua
@@ -0,0 +1,98 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- 'neovim/nvim-lspconfig'
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+  { -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/vim-vsnip-integ',
+      'onsails/lspkind.nvim',
+      'johnpapa/vscode-angular-snippets',
+      'rafamadriz/friendly-snippets'
+    },
+  },
+
+
+  { -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+
+  { 'kyazdani42/nvim-tree.lua',      dependencies = { 'kyazdani42/nvim-web-devicons' } },
+  { 'nvim-lualine/lualine.nvim',     opts = {} },
+
+
+  { 'akinsho/toggleterm.nvim',       version = '*' },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim',         opts = {} },
+
+  -- Fuzzy Finder (files, lsp, etc)
+  { 'nvim-telescope/telescope.nvim', version = '*',                                    dependencies = { 'nvim-lua/plenary.nvim' } },
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+  -- Only load if `make` is available. Make sure you have the system
+  -- requirements installed.
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    -- NOTE: If you are having trouble with this installation,
+    --       refer to the README for telescope-fzf-native for more instructions.
+    build = 'make',
+    -- build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    cond = function()
+      return vim.fn.executable 'make' == 1
+    end,
+  },
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  },
+-- Plug 'mattn/emmet-vim'
+-- Plug 'tpope/vim-surround'
+-- Plug 'wakatime/vim-wakatime'
+-- Plug '~/desktop/practice/deleteme/nvim-plugins/ngutils.nvim'
+-- Dev Tools
+-- Plug 'Equilibris/nx.nvim'
+
+})
