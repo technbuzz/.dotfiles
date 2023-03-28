2836b4014783cb4676daed54f87d6a7e42f3b6d2
technbuzz <samiullah.khan.1989@gmail.com>
2023-03-29 00:52:54 +0400

    added a lot of nice telescope features

--- lua/smk/plugins/telescope.lua
+++ lua/smk/plugins/telescope.lua
@@ -41,15 +41,26 @@ return
     local keymap = vim.keymap.set
 
     -- Find files using Telescope command-line sugar.
+    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+    vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[F]ind [R]esume' })
+    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to telescope to change theme, layout, etc.
+      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+
     keymap("n", "<leader>pf", builtin.find_files, opts)
-    keymap("n", "<leader>ff", builtin.git_files, opts)
     keymap("n", "<leader>en", "<cmd>Telescope find_files cwd=~/AppData/local/nvim<cr>", opts)
-    keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", opts)
+    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
     -- keymap("n", "<leader>fg", function()
     --   builtin.grep_string({ string = vim.fn.input("Grep > ") })
     -- end)
-    keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opts)
-    keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opts)
   end,
 }
