1150bc8729f97ca8c63e3870c343cd43e3743234
Samiullah Khan <samiullah.khan.1989@gmail.com>
2023-04-12 00:51:08 -0700

    9 cool new file tree we check neo tree (#41)
    * fixed merge conflicts
    
    * wip
    
    * added basic maping and config
    
    * added basic neotree closes #9

---
+++ lua/plug-config/neotree-config.lua
@@ -0,0 +1,26 @@
+require("neo-tree").setup({
+  default_component_configs = {
+
+    git_status = {
+      added     = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+      modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+      deleted   = "✖", -- this can only be used in the git_status source
+      renamed   = "", -- this can only be used in the git_status source
+      -- Status type
+      untracked = "",
+      ignored   = "",
+      unstaged  = "",
+      staged    = "",
+      conflict  = "",
+    },
+    window = {
+      mappings = {
+        [";"] = "open" 
+      }
+    }
+  }
+})
+
+-- keymap("n", "<C-b>", ":NvimTreeToggle<cr>", opts)
+vim.keymap.set("n", "<C-b>", ":NeoTreeRevealToggle<cr>")
+vim.keymap.set("n", "<leader>n", ":NeoTreeFocus<cr>")
