97207b395d87f7d391e068959a23967153bde033
technbuzz <samiullah.khan.1989@gmail.com>
2023-06-13 18:24:12 +0400

    missingp lugin

--- lua/lazyplugs.lua
+++ lua/lazyplugs.lua
@@ -75,6 +75,7 @@ require("lazy").setup({
 
   },
 
+  {'wellle/context.vim'},
   { 'mattn/emmet-vim' },
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
